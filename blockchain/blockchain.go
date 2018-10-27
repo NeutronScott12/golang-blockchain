5d73f35233a1848ecf1ed52a5fd513c3a0d31e03
Scott Daniel <scottberry91@gmail.com>
2018-10-27 13:14:04 +0100

    Latest Changes

---
+++ blockchain/blockchain.go
@@ -0,0 +1,90 @@
+package blockchain
+
+import (
+	"github.com/dgraph-io/badger"
+	"fmt"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
+type BlockChain struct {
+	LastHash []byte
+	Database *badger.DB
+}
+
+type BlockChainIterator struct {
+	CurrentHash []byte
+	Database *badger.DB
+}
+
+func InitBlockChain() *BlockChain {
+	var lastHash []byte
+
+	opts := badger.DefaultOptions
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err := db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+
+			genesis := Genesis()
+			fmt.Println("Genesis proved")
+			
+
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+			lastHash, err = item.Value()
+			return err
+		}
+	})
+
+	Handle(err)
+
+	blockchain := BlockChain{lastHash, db}
+
+	return &blockchain
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	var lashHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+		lastHash, err = item.Value()
+
+		return err
+	})
+
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+
+		Handle(err)
+
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.lastHash = newBlock.Hash
+
+		return err
+	})
+
+	Handle(err)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module "https://github.com/OGNeutron/golang-blockchain"
+module github.com/OGNeutron/golang-blockchain
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/AndreasBriese/bbloom v0.0.0-20180913140656-343706a395b7 // indirect
+	github.com/dgraph-io/badger v1.5.4
+	github.com/dgryski/go-farm v0.0.0-20180109070241-2de33835d102 // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+)
