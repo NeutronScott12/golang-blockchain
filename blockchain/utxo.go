1ad320d5414f5c1a67a1aec14cad8090ab54126c
Scott Daniel <scottberry91@gmail.com>
2018-11-14 17:28:33 +0000

    latest changes

---
+++ blockchain/utxo.go
@@ -0,0 +1,141 @@
+package blockchain
+
+import (
+	"encoding/hex"
+	"log"
+
+	"github.com/dgraph-io/badger"
+)
+
+var (
+	utxoPrefix   = []byte("utxo-")
+	prefixLength = len(utxoPrefix)
+)
+
+type UTXOSet struct {
+	Blockchain *BlockChain
+}
+
+func (u UTXOSet) Reindex() {
+	db := u.Blockchain.Database
+
+	u.DeleteByPrefix(utxoPrefix)
+
+	UTXO := u.Blockchain.FindUTXO()
+
+	err := db.Update(func(txn *badger.Txn) error {
+		for txId, outs := range UTXO {
+			key, err := hex.DecodeString(txId)
+
+			if err != nil {
+				return err
+			}
+
+			key = append(utxoPrefix, key...)
+
+			err = txn.Set(key, outs.Serialize())
+			Handle(err)
+		}
+
+		return nil
+	})
+
+	Handle(err)
+}
+
+func (u *UTXOSet) Update(block *Block) {
+	db := u.Blockchain.Database
+
+	err := db.Update(func(txn *badger.Txn) error {
+		for _, tx := range block.Transactions {
+			if tx.IsCoinbase() == false {
+				for _, in := range tx.Inputs {
+					updatedOuts := TxOutputs{}
+					inID := append(utxoPrefix, in.ID...)
+					item, err := txn.Get(inID)
+					Handle(err)
+					v, err := item.Value()
+					Handle(err)
+
+					outs := DeserializeOutputs(v)
+
+					for outIdx, out := range outs.Outputs {
+						if outIdx != in.Out {
+							updatedOuts.Outputs = append(updatedOuts.Outputs, out)
+						}
+					}
+
+					if len(updatedOuts.Outputs) == 0 {
+						if err := txn.Delete(inID); err != nil {
+							log.Panic(err)
+						}
+					} else {
+						if err := txn.Set(inID, updatedOuts.Serialize()); err != nil {
+							log.Panic(err)
+						}
+					}
+				}
+
+				newOutputs := TxOutputs{}
+				for _, out := range tx.Outputs {
+					newOutputs.Outputs = append(newOutputs.Outputs, out)
+				}
+
+				txID := append(utxoPrefix, tx.ID...)
+				if err := txn.Set(txID, newOutputs.Serialize()); err != nil {
+					log.Panic(err)
+				}
+			}
+		}
+
+		return nil
+	})
+
+	Handle(err)
+}
+
+func (u *UTXOSet) DeleteByPrefix(prefix []byte) {
+	deleteKeys := func(keysForDelete [][]byte) error {
+		if err := u.Blockchain.Database.Update(func(txn *badger.Txn) error {
+			for _, key := range keysForDelete {
+				if err := txn.Delete(key); err != nil {
+					return nil
+				}
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	collectSize := 100000
+	u.Blockchain.Database.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		keysForDelete := make([][]byte, 0, collectSize)
+		keysCollected := 0
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			key := it.Item().KeyCopy(nil)
+			keysForDelete = append(keysForDelete, key)
+			keysCollected++
+			if keysCollected == collectSize {
+				if err := deleteKeys(keysForDelete); err != nil {
+					log.Panic(err)
+				}
+				keysForDelete = make([][]byte, 0, collectSize)
+				keysCollected = 0
+			}
+		}
+
+		if keysCollected > 0 {
+			if err := deleteKeys(keysForDelete); err != nil {
+				log.Panic(err)
+			}
+		}
+		return nil
+	})
+}
